f838c5091d3a50377c52e69061bf4e602233123a
aurelian <aurelian@locknet.ro>
2009-01-07 21:24:35 +0100

    * added helloworld

---
+++ helloworld/controllers/bar.py
@@ -0,0 +1,7 @@
+from gaeskel.controller import Application
+
+class Bar(Application):
+
+  def hello_template(self):
+    pass
+
---
+++ helloworld/controllers/foo.py
@@ -0,0 +1,8 @@
+from gaeskel.controller import Application
+
+class Foo(Application):
+
+  def hi(self):
+    self.render_text("Har-Har")
+
+
